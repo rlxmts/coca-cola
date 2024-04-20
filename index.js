2cf008e8166f3ee76579d1c7ea7d3ae6ce87c070
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-04-19 23:13:03 -0300

    adicionando efeito no logo

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const logo = document.getElementById('logo');
-const som = new Audio('./assets/sons/sond-coca-cola.mp3')
+const som = new Audio('./assets/sons/sond-coca-cola.mp3');
+const imagemMao = document.getElementById('mao-lata');
+
 logo.addEventListener( 'click', ()=> {
     som.play();
+    imagemMao.classList.add('ativa');
 })
+
